74cad57c1372e069cc2cf912e3ecb583f368a921
its-haniish <itshaniish@gmail.com>
2025-02-06 15:22:23 +0530

    added express json

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ import './cron/updateDatabase.js';
 import { scrapeLetterboxdSearch } from './services/letterboxdScraper.js';
 import dotenv from 'dotenv';
 
+
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Database connection
 connectDB();
+
+app.use(express.json());
 
 // Error handling
 app.use((err, req, res, next) => {
